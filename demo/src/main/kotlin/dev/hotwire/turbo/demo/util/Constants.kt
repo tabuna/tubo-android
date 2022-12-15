93dc18c37799023fc630bc181a4f52712bd134ff
tabuna <bliz48rus@gmail.com>
2022-12-16 02:29:20 +0300

    init

--- demo/src/main/kotlin/dev/hotwire/turbo/demo/util/Constants.kt
+++ demo/src/main/kotlin/dev/hotwire/turbo/demo/util/Constants.kt
@@ -1,6 +1,6 @@
 package dev.hotwire.turbo.demo.util
 
-const val BASE_URL = "https://turbo-native-demo.glitch.me"
+const val BASE_URL = "https://emot.dev"
 const val HOME_URL = BASE_URL
-const val SIGN_IN_URL = "$BASE_URL/signin"
+const val SIGN_IN_URL = "$BASE_URL/login"
 const val NUMBERS_URL = "$BASE_URL/numbers"
